ff9b32b9eaa46f3e097b9a2338a98b0f1e83eae8
Jan Dobes <git@owny.cz>
2017-09-30 23:20:06 +0200

    adding description

--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 SOURCE_FILE="tlch/data/lang/English/text.big"
+
+# Checksum of text.big file from Czech version
 SOURCE_SHA256="9babf984970dd7c9caf96a615daa69e468ee1ad1efa2b79e7c7d6455ddd5c63d"
 
 SOURCE_SHA256_ACTUAL=$(sha256sum $SOURCE_FILE | awk '{print $1}')
