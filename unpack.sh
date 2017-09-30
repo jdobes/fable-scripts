089d3ee95584027e9a72559e1526da9b09e36de0
Jan Dobes <git@owny.cz>
2017-09-30 23:05:55 +0200

    add script to generate target json

---
+++ unpack.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SOURCE_FILE="tlch/data/lang/English/text.big"
+SOURCE_SHA256="9babf984970dd7c9caf96a615daa69e468ee1ad1efa2b79e7c7d6455ddd5c63d"
+
+SOURCE_SHA256_ACTUAL=$(sha256sum $SOURCE_FILE | awk '{print $1}')
+
+if [ "$SOURCE_SHA256" == "$SOURCE_SHA256_ACTUAL" ]; then
+    cat $SOURCE_FILE | ./big2json.py > texts.json
+else
+    echo "Checksum check failed."
+fi
+
+
