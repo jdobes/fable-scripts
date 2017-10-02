86e436fc81df455a4ef396e00a8ea0a8b41888c6
Jan Dobes <git@owny.cz>
2017-10-02 22:31:14 +0200

    update label test

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import re
 
 def validate_items(translation_dict):
     success = True
-    label_pattern = re.compile("^[A-Z0-9_]+$")
+    label_pattern = re.compile("^[a-zA-Z0-9_]+$")
     for key, value in translation_dict.items():
         if not label_pattern.match(key):
             print("Invalid key: '%s'" % key)
